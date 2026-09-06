39e62370d8e26ea50f58ab6f9f8b60585e13345d
Nitish Nitish <nkcode.789@gmail.com>
2026-09-06 15:51:06 +0530

    Remove TypeScript crypto helper

--- client/src/Crypto.ts
+++
@@ -1,9 +0,0 @@
-import { randomBytes } from "node:crypto";
-
-export function randomScalar32(): Buffer {
-  return randomBytes(32);
-}
-
-export function toHex(value: Buffer): string {
-  return value.toString("hex");
-}
